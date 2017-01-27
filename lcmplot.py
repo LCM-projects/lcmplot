8415364154be030b96e8b74da0fb8589255b52d4
Siyuan Feng <siyuan.feng@tri.global>
2017-01-27 17:29:08 -0500

    hookedup buttons. need to add a selector for subplot

--- lcmplot.py
+++ lcmplot.py
@@ -12,7 +12,7 @@ from matplotlib.backends.backend_qt4agg import (
     FigureCanvasQTAgg as FigureCanvas,
     NavigationToolbar2QT as NavigationToolbar)
 
-Ui_MainWindow, QMainWindow = loadUiType('window.ui')
+Ui_MainWindow, QMainWindow = loadUiType('lcmplot.ui')
 
 from flat_log import *
 
@@ -33,27 +33,58 @@ class Main(QMainWindow, Ui_MainWindow):
         self.remove_figure_button.clicked.connect(self.remove_figure_button_handler)
 
         self.fig = Figure()
-        self.idx_to_content = {111 : []}
-        self.idx_to_axe = {111 : self.fig.add_subplot(111)}
+        self.idx_to_content = {}
+        self.idx_to_axe = {}
+
+        self.current_subplot_idx = self.add_subplot()
 
 #        self.fig.subplots_adjust(left = 0.02, right = 0.99,
 #                                bottom = 0.03, top = 0.99,
 #                                wspace = 0.0, hspace = 0.03)
         self.addmpl(self.fig)
 
+    def num_of_subplots(self):
+        return len(self.idx_to_content)
+
+    def add_subplot(self):
+        new_subplot_idx = self.num_of_subplots() + 1
+        self.idx_to_content[new_subplot_idx] = []
+        self.idx_to_axe[new_subplot_idx] = self.fig.add_subplot(new_subplot_idx, 1, 1)
+
+        return new_subplot_idx
+
     def remove_figure_button_handler(self):
-        print 'remove_figure_button'
+        if self.current_subplot_idx == 1:
+            return
+
+        del_subplot_idx = self.current_subplot_idx
+        del self.idx_to_content[del_subplot_idx]
+        self.fig.delaxes(self.idx_to_axe[del_subplot_idx])
+        del self.idx_to_axe[del_subplot_idx]
+        num_subplots = self.num_of_subplots()
+
+        for subplot_idx in self.idx_to_axe:
+            self.idx_to_axe[subplot_idx].change_geometry(num_subplots, 1, subplot_idx)
+
+        self.current_subplot_idx = num_subplots
+        self.fig.canvas.draw()
 
     def add_figure_button_handler(self):
-        print 'add_figure_button'
+        self.add_subplot()
+        num_subplots = self.num_of_subplots()
+        for subplot_idx in self.idx_to_axe:
+            self.idx_to_axe[subplot_idx].change_geometry(num_subplots, 1, subplot_idx)
+
+        self.current_subplot_idx = num_subplots
+        self.fig.canvas.draw()
 
     def clear_all_button_handler(self):
-        print 'clear_all_button'
+        for subplot_idx in self.idx_to_content:
+            while self.idx_to_content[subplot_idx]:
+                self.remove_last_trace_from_subplot(subplot_idx)
 
     def clear_last_button_handler(self):
-        print 'clear_last_button'
-        subplot_idx = 111
-        self.remove_last_trace_from_fig(subplot_idx)
+        self.remove_last_trace_from_subplot(self.current_subplot_idx)
 
     def proc_log(self, log_name):
         log = lcm.EventLog(log_name, "r")
@@ -91,20 +122,19 @@ class Main(QMainWindow, Ui_MainWindow):
             is_expand = self.traceTree.expand(index)
             #self.traceTree.setExpanded(index, not(is_expand))
         else:
-            path = item.text(0)
+            path = str(item.text(0))
             while(True):
                 if not(item.parent()):
                     channel_name = str(item.text(0))
                     break
                 item = item.parent()
-                path = item.text(0) + "." + path
+                path = str(item.text(0)) + "." + path
 
-            trace_name = str(path[len(channel_name) + 1 :])
+            trace_name = path[len(channel_name) + 1 :]
 
-            subplot_idx = 111
-            self.add_trace_to_fig(subplot_idx, channel_name, trace_name)
+            self.add_trace_to_subplot(self.current_subplot_idx, channel_name, trace_name)
 
-    def add_trace_to_fig(self, subplot_idx, channel_name, trace_name):
+    def add_trace_to_subplot(self, subplot_idx, channel_name, trace_name):
         channel = self.flat_log.get_channel(channel_name)
         if not (channel.has_trace(trace_name)):
             return
@@ -117,7 +147,7 @@ class Main(QMainWindow, Ui_MainWindow):
         axe.legend()
         self.fig.canvas.draw()
 
-    def remove_last_trace_from_fig(self, subplot_idx):
+    def remove_last_trace_from_subplot(self, subplot_idx):
         axe = self.idx_to_axe[subplot_idx]
         axe.lines.pop(-1)
         self.idx_to_content[subplot_idx].pop(-1)
@@ -136,10 +166,6 @@ if __name__ == '__main__':
     import sys
     from PyQt4 import QtGui
     import numpy as np
-
-    fig1 = Figure()
-    ax1f1 = fig1.add_subplot(111)
-    ax1f1.plot(np.random.rand(5))
 
     app = QtGui.QApplication(sys.argv)
     main = Main()
